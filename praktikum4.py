6ea98c652ba243f5f40c8f70b9a7ca4007d19f17
nabilahputrii <91355858+nabilahputrii@users.noreply.github.com>
2021-10-11 14:52:31 +0700

    Add files via upload

---
+++ praktikum4.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 11 13:46:00 2021
+
+@judul : Praktikum ke-4
+@NIM : 065002100002
+@author: Nabilah Putri
+"""
+
+a = int(input("silahkan masukan angkanya : "))
+for i in range (0, a):
+    for j in range (0, a):
+        print(a, end= " ")
+    
+    a-=1
+    print(" ")
+
+
